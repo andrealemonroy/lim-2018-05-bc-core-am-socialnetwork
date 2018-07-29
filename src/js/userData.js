69ddb8ef0a462463f621b6dc1c737010aa495a2a
andrealemonroy <andreale17@icloud.com>
2018-07-29 10:20:48 -0500

    interfaz user home

--- src/js/userData.js
+++ src/js/userData.js
@@ -1,45 +1,63 @@
 document.getElementById("userProfile").addEventListener('click', () => {
+    var userId = firebase.auth().currentUser.uid;
+    const newPost = writeNewPost(userId, post.value);
+     wall.innerHTML = `<div id="wall" class="wrapper">
+     <div id="logout">
+       <p id="user_name"></p>
+       <input type="button" id="sign-off" value="Cerrar sesión">
+     </div>
+ 
+ 
+     <div class="card w-50" id="form-post">
+       <div id="photo"></div>
+       <div id="container-post" class="hiden card-body">
+ 
+         <div class="form-group ">
+           <label for="post">Publicación</label>
+           <div class="input-group ">
+             <textarea class="form-control " name="Publicación" id="post" rows="5" placeholder="¿Qué deseas publicar?"></textarea>
+             <input type="submit" value="Publicar" class="btn btn-primary" id="btn-post">
+           </div>
+           <div class="btn-group">
+             <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               •
+             </button>
+             <div class="dropdown-menu dropdown-menu-right">
+               <button class="dropdown-item" type="button">Editar</button>
+               <button class="dropdown-item" type="button">Eliminar</button>
+             </div>
+           </div>
+           <a href="#" class="btn btn-info btn-lg">
+             <span class="glyphicon glyphicon-thumbs-up"></span> Like
+           </a>
+ 
+ 
+         </div>
+       </div>
+     </div>
 
-  posts.innerHTML = `
-  <div class="card w-75" id="add-post-wrapper">
-    <div id="container-post" class="card-body">
+     <div class="card w-50">
+     <form>
+       <div>
 
-      <form id="add-form-post">
-
-        <label for="postTextArea">Publicación</label>
-          <textarea class="form-control" name="postContent" id="postTextArea" rows="5" placeholder="¿Qué deseas publicar?"></textarea>
-          <input type="checkbox" name="privatePost" value="private" />Privado
-          <button class="btn btn-primary" id="btn-add-post">Publicar</button>
-
-
-      </form>
-
-    </div>
-  </div>
-
-</div>
-
+       </div>
+     </form>
+     <div id="posts" class="hiden">
+     </div>
+   </div>
 
  `;
  posts.innerHTML += `
- <div style="display: none">
- <form id="form-edit-post" class="card w-50">
-   <fieldset>
-     <input type="hidden" name="idPost" />
-     <textarea name="postContent" required></textarea>
-     <input type="checkbox" name="privatePost" value="private" />Privado
-     <br/>
-     <button type="submit" id="btn-edit-post">Editar</button>
-   </fieldset>
- </form>
-</div>
-
-<div id="my-posts-wrappers">
-
- <br>
- <ul id="user-posts-lst">
- </ul>
-</div>`
+   <div>
+   <textarea id="${newPost}">${post.value}</textarea>
+   <div id="${posts}"><button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+   •
+  </button>            <div class="dropdown-menu dropdown-menu-right">
+  <button class="dropdown-item" type="button">Editar</button>
+  <button class="dropdown-item" type="button">Eliminar</button>
+  </div>
+  </div>
+   </div>`
  
 })
 
