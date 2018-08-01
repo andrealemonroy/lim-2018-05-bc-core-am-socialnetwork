3c237ef539a977b7c99f1ca0a0efba063124a1d0
andrealemonroy <andreale17@icloud.com>
2018-08-01 09:04:58 -0500

    prueba subir

--- src/js/main.js
+++ src/js/main.js
@@ -115,7 +115,7 @@ const validateLogin = () => {
         alert('Aún no ha activado su cuenta. Por favor ingrese a su correo para verificarla');
       } else {
         //redirect to home
-        window.location = '/src';
+        window.location = 'index.html';
       }
     }
 
